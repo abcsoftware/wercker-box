d3cc7960b417118887cfdb9fc78475b9e29b89fb
Conrad Zimmerman <me@conradz.com>
2014-12-02 10:56:22 -0500

    Cleanup

--- install.sh
+++ install.sh
@@ -1,30 +1,27 @@
 echo 'Installing core utilities'
-apt-get update -qq
-apt-get install -yqq \
+apt-get update
+apt-get install -y \
     curl \
     wget \
     git-core \
     build-essential \
     xvfb \
-    lsb-release \
-    apt-transport-https \
     python-all
 
 echo 'Installing Node.js'
 curl -sL https://deb.nodesource.com/setup | bash -
-apt-get install -yqq nodejs
+apt-get install -y nodejs
+
+echo 'Updating npm'
+npm install -g npm@latest
+npm cache clean
 
 # install firefox
 echo 'Installing Firefox'
-apt-get install -yqq firefox
+apt-get install -y firefox
 
 echo 'Installing Google Chrome'
 curl -sL https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 echo 'deb http://dl.google.com/linux/chrome/deb/ stable main' > /etc/apt/sources.list.d/google.list
-apt-get update -qq
-apt-get install -yqq google-chrome-stable
-
-echo 'Updating npm'
-which node
-which npm
-npm install -g npm@latest
+apt-get update
+apt-get install -y google-chrome-stable
