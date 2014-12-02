4f8fb15bd4d2c363f3b7c41d2a60dd8e4d0adc57
Conrad Zimmerman <me@conradz.com>
2014-12-02 08:47:32 -0500

    Better logging

--- install.sh
+++ install.sh
@@ -1,21 +1,22 @@
-# install core utilities
-apt-get update
-apt-get install -y curl wget git-core build-essential xvfb
+echo 'Installing core utilities'
+apt-get update -qq
+apt-get install -yqq curl wget git-core build-essential xvfb
 
 # install firefox
-apt-get install -y firefox
+echo 'Installing Firefox'
+apt-get install -yqq firefox
 
-# install google chrome
+echo 'Installing Google Chrome'
 curl -sL https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 echo 'deb http://dl.google.com/linux/chrome/deb/ stable main' > /etc/apt/sources.list.d/google.list
-apt-get update
-apt-get install -y google-chrome-stable
+apt-get update -qq
+apt-get install -yqq google-chrome-stable
 
-# install node.js
+echo 'Installing Node.js'
 curl -sL https://deb.nodesource.com/setup | bash -
-apt-get install -y nodejs
+apt-get install -yqq nodejs
 
-# update npm
+echo 'Updating npm'
 which node
 which npm
 npm install -g npm@latest
